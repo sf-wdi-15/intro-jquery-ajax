70a0ed9ba8cfb2087ec8f207d2e4cb531dcd45a9
jeffmorrisjr <jeff@zaarly.com>
2015-01-21 23:00:07 -0800

    fixed database

--- restaurant_app/app/controllers/restaurants_controller.rb
+++ restaurant_app/app/controllers/restaurants_controller.rb
@@ -8,6 +8,14 @@ class RestaurantsController < ApplicationController
     end
    end
 
-  def create
+def create
+@restaurant = Restaurant.create(params.require(:restaurant).permit(:name))
+
+respond_to do |f|
+  # note how for HTML requests we still want to redirect
+  f.html { redirect_to restaurants_path }
+  # we send back the new restraunt as JSON.
+  f.json { render json: @restaurant }
   end
+end
 end
