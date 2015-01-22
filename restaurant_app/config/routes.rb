3e67a4162b0668bb1fcb1afb4db9f262bc73d67a
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro-2.local>
2015-01-21 18:13:59 -0800

    initial set up, got models and controllers

---
+++ restaurant_app/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  root to: "restaurants#index"
+
+  resources :restaurants
+
+end
+
+#     restaurants GET    /restaurants(.:format)          restaurants#index
+#                 POST   /restaurants(.:format)          restaurants#create
+#  new_restaurant GET    /restaurants/new(.:format)      restaurants#new
+# edit_restaurant GET    /restaurants/:id/edit(.:format) restaurants#edit
+#      restaurant GET    /restaurants/:id(.:format)      restaurants#show
+#                 PATCH  /restaurants/:id(.:format)      restaurants#update
+#                 PUT    /restaurants/:id(.:format)      restaurants#update
+#                 DELETE /restaurants/:id(.:format)      restaurants#destroy
