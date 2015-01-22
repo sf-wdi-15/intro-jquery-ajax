1f103df507285a2a4f418eb7a60b6a87fc200670
jeffmorrisjr <jeff@zaarly.com>
2015-01-21 21:44:00 -0800

    setup

---
+++ restaurant_app/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'index/create'
+
+  root to: "restraurants#index"
+  
+  resources :users
+end
+
+#    Prefix Verb   URI Pattern               Controller#Action
+#     users GET    /users(.:format)          users#index
+#           POST   /users(.:format)          users#create
+#  new_user GET    /users/new(.:format)      users#new
+# edit_user GET    /users/:id/edit(.:format) users#edit
+#      user GET    /users/:id(.:format)      users#show
+#           PATCH  /users/:id(.:format)      users#update
+#           PUT    /users/:id(.:format)      users#update
+#           DELETE /users/:id(.:format)      users#destroy
