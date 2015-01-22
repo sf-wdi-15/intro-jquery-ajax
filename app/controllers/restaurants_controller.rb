87f527d5a1039158dca707c50de25de7ec6d4fc3
Josh Andrews <jtandr3ws37@gmail.com>
2015-01-21 20:58:49 -0800

    generate restaurants controller w/ index & create methods/views.

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,7 @@
+class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'restaurants/index'
+
+  get 'restaurants/create'
+
   root to: "restaurants#index"
   resources :restaurants
 end
