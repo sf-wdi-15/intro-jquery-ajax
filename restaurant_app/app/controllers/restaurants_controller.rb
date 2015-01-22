0fa1f65bc87d73e84465c5a7e9b32d832f2b9b2a
Samuel Cabral <samfcabral@gmail.com>
2015-01-21 18:10:53 -0800

    Initial setup

---
+++ restaurant_app/app/controllers/restaurants_controller.rb
@@ -0,0 +1,19 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    respond_to do |f|
+      f.html
+      f.json { render json: @restaurants}
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.create(params.require(:restaurant).permit(:name))
+
+    respond_to do |f|
+      f.html { redirect_to restaurants_path }
+      f.json { render json: @restaurant}
+    end
+  end
+
+end
