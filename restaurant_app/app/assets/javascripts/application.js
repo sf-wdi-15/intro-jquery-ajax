6a5203cae531c69ee32e52f619e01b0cd2caa954
Mark Daniels <mark_daniels1982@hotmail.com>
2015-01-21 22:37:22 -0800

    extra

--- restaurant_app/app/assets/javascripts/application.js
+++ restaurant_app/app/assets/javascripts/application.js
@@ -30,3 +30,55 @@ $(function () {
       console.log("The Bendix Diner", data)
     });
   });
+
+$(function () {
+  var $body = $("body");
+  $.get("/restaurants.json")
+    .done(function (restaurants) {
+      restaurants.forEach(function (restaurant) {
+        $body.append("<div>" + restaurant.name + "</div>");
+      });
+    });
+});
+
+ $(function () {
+    var $container = $("#restaurants-container");
+    $.get("/restaurants.json")
+      .done(function (restaurants) {
+        restaurants.forEach(function (restaurant) {
+          $container.append("<div>" + restaurant.name + "</div>");
+        });
+      });
+
+  });
+
+
+// Waiting for window to load
+$(function () {
+  // fetch the restaurant
+  var  $restaurantForm = $("#new_restaurant");
+
+  // wait for a submit event. 
+  // Read up on the `.on` method in JQuery docs
+  $restaurantForm.on("submit", function (event) {
+    // `event` is the event object that has info about
+    //    what was submitted
+
+    // This keeps the page from reloading.
+    event.preventDefualt();
+    alert("submitted");
+
+    // view page source to see the input
+    //  has this id. Then we grab it's value.
+    var restName = $("#restraunt_name").val();
+
+    // like before
+    $.post("/restaurants.json", {
+      restaurant: {
+        name: restName
+      }
+    }).then(function (createdRestaurant) {
+      console.log("CREATED:", createdRestaurant);
+    });
+  });
+});
