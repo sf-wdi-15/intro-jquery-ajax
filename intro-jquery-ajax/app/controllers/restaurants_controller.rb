baa351cffbb5b00e25623624e8f5708fbf453f02
Ajesh Shah <ajeshshah@gmail.com>
2015-01-22 00:59:09 -0800

    added create but not appending

--- intro-jquery-ajax/app/controllers/restaurants_controller.rb
+++ intro-jquery-ajax/app/controllers/restaurants_controller.rb
@@ -10,6 +10,18 @@ class RestaurantsController < ApplicationController
 
   def new
   end
+
+  def create
+    @restaurant = Restaurant.create(params.require(:restaurant).permit(:name))
+
+    respond_to do |f|
+      # note how for HTML requests we still want to redirect
+      f.html { redirect_to restaurants_path }
+      # we send back the new restraunt as JSON.
+      f.json { render json: @restaurant }
+    end
+  end
+
 end
 
 
