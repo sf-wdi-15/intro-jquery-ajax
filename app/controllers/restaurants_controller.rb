b61cedc78e44b8574ad76b988985a7567d67f182
Josh Andrews <jtandr3ws37@gmail.com>
2015-01-21 21:01:49 -0800

    setup restaurants controller index w/ respond_to format.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,11 @@
 class RestaurantsController < ApplicationController
   def index
+  	@restaurants = Restaurant.all
+
+  	respond_to do |f|
+  		f.html
+  		f.json { render json: @restaurants }
+  	end
   end
 
   def create
