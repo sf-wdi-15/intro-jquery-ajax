dc82a5860d9ac25c9efc980d987b1a46b1cff83b
Ajesh Shah <ajeshshah@gmail.com>
2015-01-22 00:40:58 -0800

    controller setup

--- intro-jquery-ajax/app/controllers/restaurants_controller.rb
+++ intro-jquery-ajax/app/controllers/restaurants_controller.rb
@@ -1,7 +1,15 @@
 class RestaurantsController < ApplicationController
   def index
+  	@restaurants = Restaurant.all
+
+    respond_to do |f|
+      f.html
+      f.json { render json: @restaurants }
+    end
   end
 
   def new
   end
 end
+
+
